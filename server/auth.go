c3c8159f8af454d376b297964150c1877f944f69
Danny Su <dannyrsu@gmail.com>
2019-06-20 17:01:10 -0600

    Invert the auth

--- server/auth.go
+++ server/auth.go
@@ -13,56 +13,50 @@ import (
 
 func JwtValidate(h http.Handler) http.Handler {
 	fn := func(w http.ResponseWriter, r *http.Request) {
-
-		// handleNoAuth(w, r, h)
-		noAuth := []string{
-			"/",
-			"/api/user/new",
-			"/api/user/login",
-			"/api/user/{username}/messages",
-			"/api/message",
-		}
 		requestPath := r.URL.Path
+		authRoutes := []string{
+			"/api/message/new",
+		}
 
-		for _, value := range noAuth {
+		for _, value := range authRoutes {
 			if value == requestPath {
+				tokenHeader := r.Header.Get("Authorization")
+				if tokenHeader == "" {
+					w.WriteHeader(http.StatusForbidden)
+					WriteJsonMessage(w, "Missing Token")
+					log.Fatalln("Missing token")
+					return
+				}
+
+				splitToken := strings.Split(tokenHeader, " ")
+				if len(splitToken) != 2 {
+					w.WriteHeader(http.StatusForbidden)
+					WriteJsonMessage(w, "Invalid or malformed token")
+					log.Fatalln("Invalid or malformed token")
+					return
+				}
+
+				tokenPart := splitToken[1]
+				tk := &models.Token{}
+
+				token, err := jwt.ParseWithClaims(tokenPart, tk, func(token *jwt.Token) (interface{}, error) {
+					return []byte(os.Getenv("token_password")), nil
+				})
+
+				if err != nil || !token.Valid {
+					w.WriteHeader(http.StatusForbidden)
+					WriteJsonMessage(w, "Invalid or malformed token")
+					log.Fatalf("Invalid or malformed token: %v", err)
+					return
+				}
+
+				ctx := context.WithValue(r.Context(), "user", tk.UserID)
+				r = r.WithContext(ctx)
 				h.ServeHTTP(w, r)
 				return
 			}
 		}
 
-		tokenHeader := r.Header.Get("Authorization")
-		if tokenHeader == "" {
-			w.WriteHeader(http.StatusForbidden)
-			WriteJsonMessage(w, "Missing Token")
-			log.Fatalln("Missing token")
-			return
-		}
-
-		splitToken := strings.Split(tokenHeader, " ")
-		if len(splitToken) != 2 {
-			w.WriteHeader(http.StatusForbidden)
-			WriteJsonMessage(w, "Invalid or malformed token")
-			log.Fatalln("Invalid or malformed token")
-			return
-		}
-
-		tokenPart := splitToken[1]
-		tk := &models.Token{}
-
-		token, err := jwt.ParseWithClaims(tokenPart, tk, func(token *jwt.Token) (interface{}, error) {
-			return []byte(os.Getenv("token_password")), nil
-		})
-
-		if err != nil || !token.Valid {
-			w.WriteHeader(http.StatusForbidden)
-			WriteJsonMessage(w, "Invalid or malformed token")
-			log.Fatalf("Invalid or malformed token: %v", err)
-			return
-		}
-
-		ctx := context.WithValue(r.Context(), "user", tk.UserID)
-		r = r.WithContext(ctx)
 		h.ServeHTTP(w, r)
 	}
 
